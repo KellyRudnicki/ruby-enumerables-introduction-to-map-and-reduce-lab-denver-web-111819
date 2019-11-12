615850623c2c1e1c23547c43b4dde47ee025c7f6
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-12 21:58:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -61,11 +61,11 @@ def reduce_to_all_true(source_array)
   
   while index < source_array.length do 
     if new_value = new_value + source_array[index]
-      return true
+       false
     end
     index += 1 
   end 
-  return false
+  true
 end 
 
 def reduce_to_any_true(source_array)
