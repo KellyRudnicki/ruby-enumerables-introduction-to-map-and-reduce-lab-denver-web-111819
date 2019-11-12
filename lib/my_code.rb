6ed62892d4626a55b9d9d54df938d83d3e9bdd5a
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-12 21:37:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,7 +52,7 @@ def reduce_to_total(source_array, starting_point)
     new_value += source_array[index]
     index += 1 
   end 
-  return new_value
+  return new_value(source_array,starting_point)
 end 
 
 def reduce_to_all_true(source_array)
