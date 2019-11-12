2a96978c7f740eebdd6bc7abb37705770161bb72
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-12 21:35:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,7 +45,7 @@ end
 
 
 def reduce_to_total(source_array, starting_point)
-  new_value = starting_point || 0
+  new_value = starting_point
   index = 0 
   
   while index < source_array.length do 
